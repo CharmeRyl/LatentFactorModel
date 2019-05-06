875053b2932de7d320ece15a7207c35d44791ec8
jqjiang <jjqzy@foxmail.com>
2019-05-06 08:58:26 +0800

    main app

--- recommend.py
+++ recommend.py
@@ -1,4 +1,5 @@
 from preprocessing import PreProcessing
+from lfmodel import LFModel
 
 
 def main():
@@ -6,7 +7,9 @@ def main():
     print('********** Data Preprocessing **********')
     data = PreProcessing()
     data.split_data(random=True, percentage=0.8)
-
+    print('********** Latent Factor Model **********')
+    model = LFModel(k=20, epochs=50, learning_rate=0.006, lambda_r=0.06)
+    model.train()
     pass
 
 
